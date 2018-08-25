9f3c616dbf0cc415d50cb4d2e960ac2afe096c50
Domingo Brandt <brandtyet02@gmail.com>
2018-08-25 11:38:30 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import { HomeComponent } from './home/home.component';
+import { ClientComponent } from './client/client.component';
+import { ContactComponent } from './contact/contact.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ClientComponent,
+    ContactComponent,
+    InventoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
